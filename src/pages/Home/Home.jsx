09f9446bed31ae5cd7566f8e0f3ad4be1a57434a
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-04-03 15:14:29 +0400

    adding a seeders file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import { Container } from '../../layouts'
+
+import products from './../../seeders/Products'
 
 const HomePage = () => {
   return (
     <Container>
-      <h1 className='ui-title-1'>Home Page</h1>
+      <div className='flex flex-wrap'>
+        {products.map((item) => (
+          <div
+            className='ui-card isAnimated mr-4 mb-4'
+            key={item.id}
+            style={{ maxWidth: '420px' }}
+          >
+            <div className='ui-card-body'>
+              <img
+                src={item.img}
+                alt={item.title}
+                style={{ maxWidth: '320px' }}
+              />
+              <div className='flex flex-col items-center'>
+                <span className='ui-title-3 mb-2'>{item.title}</span>
+                <span className='mb-4'>{item.price}</span>
+                <Link to={`/${item.alias}`}>
+                  <div className='ui-button isPrimary'>See more</div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </Container>
   )
 }
