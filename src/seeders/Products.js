b8b0b1296054e5484bffa63f4fa83b8b34c74dea
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-04-03 15:15:35 +0400

    edited siders

---
+++ src/seeders/Products.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    alias: 'stormtroper-helmet',
+    title: 'STORMTROPER HELMET',
+    price: '$299',
+    img: require('../assets/img/products/product-1.png'),
+  },
+  {
+    id: 2,
+    alias: 'imperial-armys-tie-fighter',
+    title: 'IMPERIAL ARMY’S TIE FIGHTER',
+    price: '$2.499',
+    img: require('../assets/img/products/product-2.png'),
+  },
+  {
+    id: 3,
+    alias: 'kylo-rens-lightsaber',
+    title: 'KYLO REN’S LIGHTSABER',
+    price: '$1.349',
+    img: require('../assets/img/products/product-3.png'),
+  },
+  {
+    id: 4,
+    alias: 'imperial-armys-death-star',
+    title: 'IMPERIAL ARMY’S DEATH STAR',
+    price: '$1.299',
+    img: require('../assets/img/products/product-4.png'),
+  },
+]
