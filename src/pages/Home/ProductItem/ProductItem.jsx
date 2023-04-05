987f9746a53931b18c72295b1d299df929fd6d1a
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-04-05 15:39:47 +0400

    We make indicators of loading and successful addition to the basket

--- src/pages/Home/ProductItem/ProductItem.jsx
+++ src/pages/Home/ProductItem/ProductItem.jsx
@@ -2,10 +2,16 @@ import React, { useEffect, useState } from 'react'
 import { Link, useParams, useNavigate } from 'react-router-dom'
 import products from 'seeders/Products'
 import { Container } from '../../../layouts'
+import classNames from 'classnames'
 
 const ProductItemPage = () => {
   const [item, setItem] = useState(true)
+//status
+  const [loading, setLoading] = useState(false)
+  const [success, setSuccess] = useState(false)
+  //alias
   const { itemAlias } = useParams()
+  //redirect
   const navigate = useNavigate()
   useEffect(() => {
     const nextItem = products.find((el) => el.alias === itemAlias)
@@ -16,17 +22,33 @@ const ProductItemPage = () => {
       navigate('/')
     }
   }, [])
+
+  const handleAddCard = () => {
+    //....logic
+    setLoading(true)
+    setTimeout(() => {
+      setSuccess(true)
+      setLoading(false)
+    }, 1000);
+  }
+
+
+    const buttonClasses = classNames('ui-button isPrimary', {
+      isLoading: loading,
+    })
+
   const renderContent = item && (
     <div className='flex flex-col items-center'>
       <img src={item.img} alt={item.title} style={{ maxWidth: '420px' }} />
       <h1 className='ui-title-1 mb-4'>{item.title}</h1>
       <span>{item.price}</span>
       {/* controls */}
-      <div className='flex mt-4'>
+      {success && <p className='ui-text isSuccess mt-4'>Success! Product added to the cart!</p>}
+      <div className='flex mt-2'>
         <Link className='ui-button isLink' to='/'>
           Add to home
         </Link>
-        <Link className='ui-button isPrimary'>Add to cart</Link>
+        <Link className={buttonClasses} onClick={handleAddCard}>{loading ? 'loading...' : 'add to cart'}</Link>
       </div>
     </div>
   )
