9a99978055f4e99e0394a538ceddedff496d2bb6
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-04-08 17:43:08 +0400

    Removing products from the store

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,17 +1,63 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
-
+///store
+import { ProductsContext } from 'Store'
 import { Container } from '../../layouts'
 
+const productStyle = {
+  width: '100%',
+  maxWidth: '520px',
+  padding: '10px',
+  backgroundColor: 'var(--ui-g-100)',
+  borderRadius: '16px',
+}
+
 const CheckoutPage = () => {
+  const [products, setProducts] = useContext(ProductsContext)
+
+  //content
+  const renderContent =
+    products && products.length && products.length > 0
+      ? products.map((item, i) => (
+          <div
+            key={i}
+            className='flex justify-center items-center mb-8'
+            style={productStyle}
+          >
+            <img
+              src={item.img}
+              alt={item.title}
+              style={{ maxWidth: '140px' }}
+            />
+            <div className='flex flex-col items-start'>
+              <p className='ui-title-3 mb-2'>{item.title}</p>
+              <p className='mb-4'>{item.price}</p>
+              <Link to={`/products/${item.alias}`}>
+                <span className='ui-link isPrimary'>See more</span>
+              </Link>
+            </div>
+          </div>
+        ))
+      : 'Cart is empty'
+
+  const renderControls = (
+    <div className='flex mt-4'>
+      <Link className='ui-button isLink' to='/'>
+        Back to home
+      </Link>
+      {products && products.length > 0 && (
+        <div className='ui-button isPrimary' onClick={() => setProducts([])}>
+          Refresh to cart
+        </div>
+      )}
+    </div>
+  )
   return (
     <Container>
-      <h1 className='ui-title-1 mb-4'>Checkout</h1>
-      <div className='flex mt-4'>
-        <Link className='ui-button isLink' to='/'>
-          Back to home
-        </Link>
-        <Link className='ui-button isPrimary'>Refresh to cart</Link>
+      <div className='flex flex-col items-center'>
+        <h1 className='ui-title-1 mb-4'>Checkout</h1>
+        {renderContent}
+        {renderControls}
       </div>
     </Container>
   )
